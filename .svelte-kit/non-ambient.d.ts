34e4110842b39dbca4694f102f53515d5d564fec
suanna0 <117539182+suanna0@users.noreply.github.com>
2026-03-11 22:26:01 -0400

    folder

---
+++ .svelte-kit/non-ambient.d.ts
@@ -0,0 +1,48 @@
+
+// this file is generated — do not edit it
+
+
+declare module "svelte/elements" {
+	export interface HTMLAttributes<T> {
+		'data-sveltekit-keepfocus'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-noscroll'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-preload-code'?:
+			| true
+			| ''
+			| 'eager'
+			| 'viewport'
+			| 'hover'
+			| 'tap'
+			| 'off'
+			| undefined
+			| null;
+		'data-sveltekit-preload-data'?: true | '' | 'hover' | 'tap' | 'off' | undefined | null;
+		'data-sveltekit-reload'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-replacestate'?: true | '' | 'off' | undefined | null;
+	}
+}
+
+export {};
+
+
+declare module "$app/types" {
+	export interface AppTypes {
+		RouteId(): "/" | "/admin" | "/api" | "/api/choice" | "/api/events" | "/api/join" | "/api/resolve" | "/play";
+		RouteParams(): {
+			
+		};
+		LayoutParams(): {
+			"/": Record<string, never>;
+			"/admin": Record<string, never>;
+			"/api": Record<string, never>;
+			"/api/choice": Record<string, never>;
+			"/api/events": Record<string, never>;
+			"/api/join": Record<string, never>;
+			"/api/resolve": Record<string, never>;
+			"/play": Record<string, never>
+		};
+		Pathname(): "/" | "/admin" | "/api/choice" | "/api/events" | "/api/join" | "/api/resolve" | "/play";
+		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
+		Asset(): "/robots.txt" | string & {};
+	}
+}
